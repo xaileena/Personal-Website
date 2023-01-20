e2b2518d6e785f3769cab50c30006002381d91e6
Aileena Xie <aileena0925@gmail.com>
2023-01-19 22:44:07 -0500

    Changed the font of my name

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,15 @@
+import { Lobster } from '@next/font/google';
+
+const lobster = Lobster({ subsets: ['latin'], weight: '400' });
+
 export default function Home() {
   return (
     <div className="h-screen flex flex-col justify-center bg-rose-200">
       <div className="flex flex-row justify-center">
         <div className="border-solid border-8 border-red-300 py-40 w-2/3 flex justify-center bg-pink-100">
-          <h1 className="text-pink-400 text-5xl">Aileena Xie</h1>
+          <h1 className={`text-pink-400 text-8xl ${lobster.className}`}>
+            Aileena Xie
+          </h1>
         </div>
       </div>
     </div>
